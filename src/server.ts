1447ffd769840f927c44e8195e66968fc1e987eb
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-03-30 19:24:08 -0500

    starting apollo server

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,24 @@ import express from "express"
 import compression from "compression"
 import cors from "cors"
 import { createServer } from "http";
+import { ApolloServer } from "apollo-server-express"
+import schema from "./schema"
+import expressPlayGround from "graphql-playground-middleware-express"
 
 const app = express();
 app.use("*", cors());
 app.use(compression());
 
-app.get("/", (req, res) => {
-    res.send("Hola con graphql")
+const servivor = new ApolloServer({
+    schema,
+    introspection: true
+
 })
+
+servivor.applyMiddleware({ app });
+app.get("/", expressPlayGround({
+    endpoint: '/graphql'
+}))
 
 const httpServer = createServer(app);
 
@@ -17,5 +27,5 @@ const PORT = 5300
 httpServer.listen({
     port: PORT
 },
-    () => console.log(`Funcionando en http://localhost:${PORT}`)
+    () => console.log(`Funcionando en http://localhost:${PORT}/graphql`)
 )
