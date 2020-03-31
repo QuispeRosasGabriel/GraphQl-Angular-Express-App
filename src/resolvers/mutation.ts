46dc5c88b0a3e1dba5e3a7e813af29bfbfc9f117
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-03-31 18:06:58 -0500

    push function

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,7 +1,25 @@
 import { IResolvers } from "graphql-tools";
 import _ from 'lodash';
+import { database } from "../data/data.store";
 const mutation: IResolvers = {
-
+    Mutation: {
+        cursoNuevo(__: void, { curso }): any {
+            const ItemCurso = {
+                id: String(database.cursos.length + 1),
+                title: curso.title,
+                description: curso.description,
+                clases: curso.clases,
+                time: curso.time,
+                logo: curso.logo,
+                level: curso.level,
+                path: curso.path,
+                teacher: curso.teacher,
+                reviews: []
+            }
+            database.cursos.push(ItemCurso);
+            return ItemCurso
+        }
+    }
 }
 
 export default mutation
